89eafe334f9b72567b5c1246924fe010050149b8
isaacs <i@izs.me>
2017-03-10 14:26:57 -0800

    factor out running the child process to foreground-child

--- bin.js
+++ bin.js
@@ -1,5 +1,2 @@
-require('child_process')
-.spawn(process.argv[0],
-       process.argv.slice(1),
-       { customFds: [ 0, 1, 2 ] })
-.on('exit', process.exit)
+import fg from 'foreground-child'
+fg(process.argv.slice(2))
