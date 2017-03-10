eb859a7c4272e8f180e324fcbf2f7fe742a55457
isaacs <i@izs.me>
2017-03-10 14:38:52 -0800

    Much more comprehensive tests, with impressive coverage

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 
-gulp.task('default', (...args) =>
+gulp.task('default', (...args) => {
     gulp.src('bin.js')
         .pipe(babel({
           "plugins": [
@@ -16,4 +16,21 @@ gulp.task('default', (...args) =>
             "es2017"
           ]
         }))
-        .pipe(gulp.dest('dist')))
+        .pipe(gulp.dest('dist'))
+
+  gulp.src('test-src/*.js')
+      .pipe(babel({
+          "plugins": [
+            [
+              "transform-es2015-modules-commonjs",
+              {
+                "allowTopLevelThis": true
+              }
+            ]
+          ],
+          "presets": [
+            "es2017"
+          ]
+        }))
+        .pipe(gulp.dest('test'))
+})
