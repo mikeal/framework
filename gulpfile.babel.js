431f1139da5b8d963dae59457f93778c614c81e6
isaacs <i@izs.me>
2017-03-10 14:22:12 -0800

    Support new language features
    This brings framework up to date with the latest and greatest in ES2017
    features.
    
    Now at last we can feel free to use MODERN code in this cutting-edge
    framework, with a minimum amount of hassle.

---
+++ gulpfile.babel.js
@@ -0,0 +1,19 @@
+import gulp from 'gulp'
+import babel from 'gulp-babel'
+
+gulp.task('default', (...args) =>
+    gulp.src('bin.js')
+        .pipe(babel({
+          "plugins": [
+            [
+              "transform-es2015-modules-commonjs",
+              {
+                "allowTopLevelThis": true
+              }
+            ]
+          ],
+          "presets": [
+            "es2017"
+          ]
+        }))
+        .pipe(gulp.dest('dist')))
